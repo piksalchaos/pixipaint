15e067da7824b2c0a59fee89d4a6456726e6f205
Justin Le <108901892+piksalchaos@users.noreply.github.com>
2025-07-25 21:40:05 -0700

    tiles now outline themselves to show mistakes

--- app/components/game/tile.tsx
+++ app/components/game/tile.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useEffect, useState } from "react";
 import { getColorStyleValue } from "../../lib/color-style";
 import styles from "./game.module.css";
 
@@ -16,18 +18,32 @@ export function Tile({
   handleMistake: () => void;
   handleFilled: (index: number) => void;
 }) {
+  const [hadMistake, sethadMistake] = useState(false);
+  useEffect(() => {
+    if (hadMistake) {
+      const timer = setTimeout(() => {
+        sethadMistake(false);
+      }, 400);
+      return () => {
+        if (timer) clearTimeout(timer);
+      };
+    }
+  });
+
   const handleClick = () => {
     if (isFilled) return;
     if (chosenColorId === colorId) {
       handleFilled(index);
       return;
     }
+    sethadMistake(true);
     handleMistake();
   };
   return (
     <div
       className={styles.tile}
       style={{
+        borderColor: hadMistake ? "red" : "transparent",
         backgroundColor: isFilled
           ? getColorStyleValue(colorId)
           : "var(--background)",
